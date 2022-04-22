4cde0f0ef708ecc6a4adbf539b268a9214b212e2
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-04-22 15:59:51 -0400

    added HTML code for new.ejs file

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,2 +1,11 @@
+function newMovie(req, res){
+    // this is a template
+    // we will render when the
+    // user visits
+    // http://localhost:3000/new
+    res.render('movies/new');
+}
 
-
+module.exports = {
+    new : newMovie
+}
