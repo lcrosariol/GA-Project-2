42a7f17c052deba9237c8c53f92fc7048ab1cbc4
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-04-22 16:46:26 -0400

    working movie form

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,11 +1,34 @@
+// we need to import movie Schema, then only we can create the movie object
+const Movie = require('../models/movie');
+
 function newMovie(req, res){
     // this is a template
     // we will render when the
     // user visits
-    // http://localhost:3000/new
+    // http://localhost:3000/movies/new
     res.render('movies/new');
 }
 
+function create(req, res){
+    // here to perform the data clean up
+    req.body.nowShowing = !!req.body.nowShowing;
+    req.body.cast = req.body.cast.trim();
+    if(req.body.cast) req.body.cast.split(/\s*,\s*/);
+
+    // we crate the movie object
+    const  movie = new Movie(req.body);
+
+    // we save the movie object to the db
+    movie.save(function (error){
+        if(error) return res.render('movies/new');
+        console.log(movie);
+        res.redirect('movies/new')
+    });
+
+
+}
+
 module.exports = {
-    new : newMovie
+    new : newMovie,
+    create
 }
--- routes/movies.js
+++ routes/movies.js
@@ -4,7 +4,13 @@ var router = express.Router();
 // controllers = business logic
 const  movieCtrl = require('../controllers/movies');
 
-// if user visits http://localhost:3000/new
+// if user visits http://localhost:3000/movies/new
 router.get('/new', movieCtrl.new);
+
+// to process new movie form data
+// means, we need to send the data to the server
+// using POST method
+
+router.post('/', movieCtrl.create);
 
 module.exports = router;
