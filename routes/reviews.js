8fc07b1374a1287e24111f04fe05f2b7e08f6577
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-04-25 15:08:43 -0400

    Create and Code the Controller Action

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const reviewCtrl = require('../controllers/reviews');
 
 // http://localhost:3000/movies = movie controller
-// http://localhost:3000/ = reviews controller
 
 // http://localhost:3000/movies/6263111741a184f9cf657813
 router.post('/movies/:id/reviews', reviewCtrl.create);
