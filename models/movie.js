383d683c2255ae712e5fe6465bf8aa0383d9e95c
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-04-25 13:48:08 -0400

    Creating a Schema for the Review Subdocuments

--- models/movie.js
+++ models/movie.js
@@ -1,6 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+
+const reviewSchema = new Schema({
+  content: String,
+  rating : { type: Number, min: 1, max : 5, default:5}
+  }, {
+    timestamps: true
+});
+
 var movieSchema = new Schema({
   title: {
     type: String,
@@ -13,7 +21,9 @@ var movieSchema = new Schema({
     }
   }, mpaaRating: String,
   cast: [String],
-  nowShowing: { type: Boolean, default: false }
+  nowShowing: { type: Boolean, default: false },
+  // this means, movie can have many reviews
+  reviews: [reviewSchema]
 }, {
   timestamps: true
 });
