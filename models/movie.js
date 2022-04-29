638a84407dd28eb6b6f5b1bc65122a31aa30595b
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-04-29 17:02:11 -0400

    updated reviewSchema with the user model

--- models/movie.js
+++ models/movie.js
@@ -3,7 +3,10 @@ const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
   content: {type: String, required: true},
-  rating: {type: Number, min: 1, max: 5, default: 5}
+  rating: {type: Number, min: 1, max: 5, default: 5},
+  user: {type: Schema.Types.ObjectId, ref: 'User'},
+  userName: String,
+  userAvatar: String
 }, {
   timestamps: true
 });
