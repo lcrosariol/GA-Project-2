8ad2c1c7e48100ce36f4c713f8fb2b9526c8208b
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-04-29 16:04:14 -0400

    Step 8.1 passport.use

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,17 @@
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(
+    new GoogleStrategy(
+        // Configuration object
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+            callbackURL: process.env.GOOGLE_CALLBACK
+        },
+        // Verify callback function
+        function(accessToken, refreshToken, profile, cb) {
+            // a user has logged in with OAuth...
+        }
+    )
+);
