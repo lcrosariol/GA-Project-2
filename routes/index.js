e4746b7dee56a29149c2124607c9176c448280c2
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-04-29 16:37:46 -0400

    Step 9.1 Coding the Routes...

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
 
 router.get('/', function(req, res, next) {
   res.redirect('/movies');
