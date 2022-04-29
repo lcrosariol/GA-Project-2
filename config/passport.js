944775774e14d541bfb275b29877e71a6340c4ce
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-04-29 16:20:04 -0400

    Step 8.4 - Verify Callback Code

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('../models/user');
 
 passport.use(
     new GoogleStrategy(
@@ -9,9 +10,25 @@ passport.use(
             clientSecret: process.env.GOOGLE_SECRET,
             callbackURL: process.env.GOOGLE_CALLBACK
         },
-        // Verify callback function
+        // The verify callback function
         function(accessToken, refreshToken, profile, cb) {
             // a user has logged in with OAuth...
+            User.findOne({ googleId: profile.id }).then(async function(user) {
+                if (user) return cb(null, user);
+                // We have a new user via OAuth!
+                try {
+                    user = await User.create({
+                        name: profile.displayName,
+                        googleId: profile.id,
+                        email: profile.emails[0].value,
+                        avatar: profile.photos[0].value
+                    });
+                    return cb(null, user);
+                } catch (err) {
+                    return cb(err);
+                }
+            });
         }
     )
 );
+
