209251c6b8d7645c40366715d1ff282739f00144
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-04-29 16:42:18 -0400

    Step 9.3 Google Callback Route

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,13 @@ router.get('/auth/google', passport.authenticate(
     { scope: ['profile', 'email'] }
 ));
 
+router.get('/oauth2callback', passport.authenticate(
+    'google',
+    {
+      successRedirect : '/movies',
+      failureRedirect : '/movies'
+    }
+));
+
+
 module.exports = router;
